135f9ebb631ff5df5d43cdd73e99379dc72cddef
VictorIborTech <iborvictor2001@gmail.com>
2024-03-04 10:11:53 +0100

    commit

--- drinks/drinks/models.py
+++ drinks/drinks/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Drink(models.Model):
     name = models.CharField(max_length=250)
     decription = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.name
